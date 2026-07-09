f887fc25d0cb1b9de7b13e7bf8dcca1c9a6579de
schokett <skrollanelenazimmermann@gmail.com>
2026-07-09 16:42:59 +0200

    changed style from boardOverview

--- src/pages/BoardOverview.tsx
+++ src/pages/BoardOverview.tsx
@@ -5,7 +5,7 @@ import { Card, CardAction, CardDescription, CardTitle } from "@/components/ui/ca
 
 function BoardOverview() {
   return (
-    <section className="w-full max-w-300 mx-auto px-4 sm:px-6 lg:px-8">
+    <section className="w-full max-w-300 mx-auto px-4 sm:px-6 lg:px-8 ">
       <div className="flex flex-col gap-2 mt-5 justify-center text-center sm:flex-row items-center sm:justify-between">
         <h2 className="font-bold ">Meine Boards</h2>
         <Button className={"w-fit"}>
@@ -14,22 +14,24 @@ function BoardOverview() {
         </Button>
       </div>
 
-      <Card className="w-full max-w-sm flex-2 flex flex-row justify-between p-4">
-        <div className="w-full">
-          <CardTitle>Board</CardTitle>
-          <CardDescription>
-            <span className="">3 Spalten • </span>
-            <span className="">0 Tasks</span>
-          </CardDescription>
-        </div>
-        <div className="w-fit">
-          <CardAction>
-            <Button variant="link">
-              <TrashIcon />
-            </Button>
-          </CardAction>
-        </div>
-      </Card>
+      <section className="place-items-center lg:place-items-start">
+        <Card className="w-full max-w-sm flex-2 flex flex-row justify-between p-4 mt-5">
+          <div className="w-full">
+            <CardTitle>Board</CardTitle>
+            <CardDescription>
+              <span className="">3 Spalten • </span>
+              <span className="">0 Tasks</span>
+            </CardDescription>
+          </div>
+          <div className="w-fit">
+            <CardAction>
+              <Button variant="link">
+                <TrashIcon />
+              </Button>
+            </CardAction>
+          </div>
+        </Card>
+      </section>
 
       <div className="text-center flex justify-center mt-12 sm:mt-30">
         <p className="text-slate-500 font-medium">
