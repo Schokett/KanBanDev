2d13ee44ebc97f8d5fcc617c66db99a24078e963
schokett <skrollanelenazimmermann@gmail.com>
2026-07-13 10:05:19 +0200

    link on logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { CircleUserRound } from "lucide-react";
 import { Button } from "@base-ui/react";
 
@@ -6,25 +6,27 @@ function App() {
   return (
     <>
       <div className="bg-black h-20 flex justify-between items-center p-5">
-        <h1 className="text-cyan-300 font-bold text-xl flex gap-2 items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-layout-dashboard-icon lucide-layout-dashboard">
-            <rect width="7" height="9" x="3" y="3" rx="1" />
-            <rect width="7" height="5" x="14" y="3" rx="1" />
-            <rect width="7" height="9" x="14" y="12" rx="1" />
-            <rect width="7" height="5" x="3" y="16" rx="1" />
-          </svg>
-          KanBanDev
-        </h1>
+        <Link to={`/boardoverview`}>
+          <h1 className="text-cyan-300 font-bold text-xl flex gap-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-layout-dashboard-icon lucide-layout-dashboard">
+              <rect width="7" height="9" x="3" y="3" rx="1" />
+              <rect width="7" height="5" x="14" y="3" rx="1" />
+              <rect width="7" height="9" x="14" y="12" rx="1" />
+              <rect width="7" height="5" x="3" y="16" rx="1" />
+            </svg>
+            KanBanDev
+          </h1>
+        </Link>
         <Button className="text-center items-center flex text-slate-100 gap-2">
           <CircleUserRound className="text-gray-400" stroke-width="2" size={20} />
           Nutzer
